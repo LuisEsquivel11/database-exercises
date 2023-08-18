aad5ef0e750dce84717576e33c48b3705c549e13
Luis <LuisDEsquivel11@gmail.com>
2023-08-18 09:08:34 -0500

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_tests_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(50) DEFAULT 'NONE',
+    release_date INT DEFAULT 'NONE',
+    sales FLOAT DEFAULT 'NONE',
+    genre VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+);
